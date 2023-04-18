f32f052caefb374cc2eddc16eb8119d7c916f976
Ignacio Serrano Rodriguez <ignacioserranorodriguez@gmail.com>
2023-04-18 10:22:01 +0200

    Ejercicio 6 finalizado (correcion errores)

--- 6_vocales_consonantes.sh
+++ 6_vocales_consonantes.sh
@@ -3,20 +3,16 @@
 # 6. Escribir un script que pida al usuario una cadena de texto y luego imprima el
 # número de vocales y consonantes que contiene.
 
-<<<<<<< HEAD
 vocales=("a" "e" "i" "o" "u")
 consonantes=("q" "w" "r" "t" "y" "p" "s" "d" "f" "g" "h" "j" "k" "l" "ñ" "z" "x" "c" "v" "b" "n" "m")
 contadorVocales=0
 contadorConsonantes=0
 
 read -p "Introduce una cadena de texto: " cadena
-=======
->>>>>>> 9dea415fe8888b83b3a30c9c9df39306bab4cd6f
 
 for (( i=0; i<${#cadena}; i++ ))
 do
     caracter="${cadena:$i:1}"
-<<<<<<< HEAD
     
     for vocal in ${vocales[@]}
     do
@@ -37,5 +33,3 @@ do
 done
 
 echo -e "Numero de vocales = $contadorVocales\nNumero de consonantes = $contadorConsonantes"
-=======
->>>>>>> 9dea415fe8888b83b3a30c9c9df39306bab4cd6f

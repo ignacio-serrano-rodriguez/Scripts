8095c45f46854e5cb478e314423fdf7a133fea5c
Ignacio Serrano Rodriguez <ignacioserranorodriguez@gmail.com>
2023-04-18 16:57:29 +0200

    Ejercicio 1 y 2 Administracion terminado. Ejercicio 3 inicio

---
+++ Administracion/2_procesos_root.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+# 2. Muestra los procesos en ejecución de root.
+
+clear
+echo -e "Se ha ejecutado el comando (ps -u root)\n"
+ps -u root
---
+++ Administracion/3_procesos_programa.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+# 3. Listar los procesos que están ejecutando un determinado programa.
+
+echo 'soy el script 3 de administracion'

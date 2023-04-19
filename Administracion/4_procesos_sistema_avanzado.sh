19d533a3fd34556a83bc575bac8032ddb058f6af
Ignacio Serrano Rodriguez <ignacioserranorodriguez@gmail.com>
2023-04-19 12:01:29 +0200

    Ejercicio 4 Administracion terminado.

--- Administracion/4_procesos_sistema_avanzado.sh
+++ Administracion/4_procesos_sistema_avanzado.sh
@@ -3,4 +3,16 @@
 # 4. Muestra el PID, PPID, uso de la memoria, tiempo de CPU y el nombre del
 # proceso de cada proceso.
 
-ps -C $1  
+# PID (pid)
+# PPID (ppid)
+# uso memoria (size) ???
+# tiempo CPU (pcpu)
+# nombre 
+
+clear
+echo -e "Se ha ejecutado el comando (ps -eo pid,ppid,size,pcpu,cmd)\n"
+ps -eo pid,ppid,size,pcpu,cmd
+
+echo ''
+read -p "Pulsa intro para continuar "
+clear

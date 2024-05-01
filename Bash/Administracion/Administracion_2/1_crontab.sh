c22810ab927b1e2f626242abab96e0c265fcfbd2
Ignacio Serrano Rodríguez <ignacioserranorodriguez@gmail.com>
2024-05-01 16:42:05 +0200

    Delete Bash directory

--- Bash/Administracion/Administracion_2/1_crontab.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# 1. 1. Añade una tarea al fichero crontab que guarde en un fichero la fecha y hora del
-# sistema. Se tiene que repetir cada minuto de los jueves de cada semana.
-
-# Ejecutamos el comando: crontab -e
-# Se ejecuta nano para el fichero /var/spool/cron/crontabs/usuaio
-# Añadimos la siguiente linea al final del fichero:
-    # 

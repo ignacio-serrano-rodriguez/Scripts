77ff0c3b66098e6a814dea9685cbf5071b038e35
Ignacio Serrano Rodríguez <ignacioserranorodriguez@gmail.com>
2024-06-04 19:39:10 +0200

    Changed to show display server

--- startupTerminal.bash
+++ startupTerminal.bash
@@ -1,2 +1,2 @@
 #!/bin/bash
-gnome-terminal --command="bash -i -c 'cd ~; neofetch; upd; exec bash'"
+gnome-terminal --command="bash -i -c 'cd ~; neofetch; echo Display server: $XDG_SESSION_TYPE; echo; upd; exec bash'"
